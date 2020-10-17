b8cf55fb0fa1bbca79e5f83bc433ff1f4545d873
ash12-coder <68694966+ash12-coder@users.noreply.github.com>
2020-10-17 11:46:29 +0530

    Add files via upload

---
+++ left2.js
@@ -0,0 +1,18 @@
+function maxwidth(){
+    document.getElementById("main").style.width = "250px";
+    document.getElementById("main").style.transition = "1s";
+    document.getElementById("a").style.visibility = "visible";
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("c").style.visibility = "visible";
+    document.getElementById("d").style.visibility = "visible";
+    document.getElementById("e").style.visibility = "visible";}
+function minwidth(){
+    document.getElementById("main").style.width = "70px";
+    document.getElementById("main").style.transition = "1s";
+    document.getElementById("a").style.visibility = "hidden";
+    document.getElementById("b").style.visibility = "hidden";
+    document.getElementById("c").style.visibility = "hidden";
+    document.getElementById("d").style.visibility = "hidden";
+    document.getElementById("e").style.visibility = "hidden";
+
+}
